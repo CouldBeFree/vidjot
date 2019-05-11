aa57b8af0c2030a07180d7fc9c751a9e711ec2eb
CouldBeFree <propokerstar90@gmail.com>
2019-05-11 21:05:55 +0300

    added handlebars

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
 const app = express();
 
-app.use('/', (req, res) => {
-   res.send('Hello');
+// Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Index route
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+// About route
+app.get('/about', (req, res) => {
+    res.send('ABOUT')
 });
 
 const port = 5000;
