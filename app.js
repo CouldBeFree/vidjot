5cb08e6209ae3cf63d826e3790c9878b1b63828e
CouldBeFree <propokerstar90@gmail.com>
2019-05-14 20:57:55 +0300

    modifyied

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.Promise= global.Promise;
 
 // Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
-    useMongoClient: true
+    useNewUrlParser: true
 })
     .then(() => {
         console.log('MongoDB connected')
     })
     .catch(err => console.log(err));
+
+// Connect model idea
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -21,6 +30,51 @@ app.set('view engine', 'handlebars');
 // Index route
 app.get('/', (req, res) => {
     res.render('index')
+});
+
+// Process form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({ text:'Please add a title' })
+    }
+    if(!req.body.details){
+        errors.push({ text:'Please add a details' })
+    }
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        })
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        };
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas')
+            })
+    }
+});
+
+// Add idea form
+app.get('/ideas/add', (req, res) => {
+   res.render('ideas/add');
+});
+
+// Idea index page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({date:'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            })
+        });
 });
 
 // About route
